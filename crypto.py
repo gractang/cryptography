000f1cf9a152cf495625f248b806c8d63c3b06f4
gractang <purple.grace.t@gmail.com>
2020-09-22 14:30:10 -0700

    vigenere encryption method

--- crypto.py
+++ crypto.py
@@ -3,6 +3,8 @@ import string
 
 # Caesar Cipher
 
+# Builds a corresponding shifted "alphabet"
+# to reference in Caesar cipher encoder
 def build_corresponding(offset):
     normal = string.ascii_uppercase
     shifted = normal[offset:] + normal[:offset]
@@ -20,22 +22,45 @@ def encrypt_caesar(plaintext, offset):
             ciphertext += ch
     return ciphertext
 
-
 # Arguments: string, integer
 # Returns: string
 def decrypt_caesar(ciphertext, offset):
     return encrypt_caesar(ciphertext, -offset)
 
 # Vigenere Cipher
+
+# Makes a key— truncated if keyword is longer than plaintext,
+# padded if keyword is shorter
+def make_key(plaintext, keyword):
+    key = ""
+    pos = 0
+    while len(plaintext) > len(key):
+        key += keyword[pos]
+        pos += 1
+        pos %= len(keyword)
+    return key
+
 # Arguments: string, string
 # Returns: string
 def encrypt_vigenere(plaintext, keyword):
-    pass
+    key = make_key(plaintext, keyword)
+    pos = 0
+    ciphertext = ""
+    for ch in plaintext:
+        offset = ord(key[pos])-ord('A')
+        index = (ord(ch) + offset)
+        ch_new = chr(index) if index-ord('A') < 26 else chr(index-26)
+        ciphertext += ch_new
+        pos += 1
+    return ciphertext
+
 
 # Arguments: string, string
 # Returns: string
 def decrypt_vigenere(ciphertext, keyword):
     pass
+
+
 
 # Merkle-Hellman Knapsack Cryptosystem
 # Arguments: integer
@@ -60,9 +85,9 @@ def decrypt_mhkc(ciphertext, private_key):
 
 def main():
     # Testing code here
-    print(encrypt_caesar("PYTHON!", 3))
-    print(decrypt_caesar("SBWKRQ", 3))
-    pass
+    # print(encrypt_caesar("PYTHON!", 3))
+    # print(decrypt_caesar("SBWKRQ", 3))
+    print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
 
 if __name__ == "__main__":
     main()
